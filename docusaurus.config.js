fc5e6a105e0ee38b6b53f0c59122ab492d15b6ab
MbassiJaphet <mbassijaphet@gmail.com>
2020-10-21 22:38:48 +0100

    Revert "Update docusaurus.config.js"
    This reverts commit 67d2b0a4cc5a443e970ec3fa7bcc05a34369906f.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Pytorch For Information Extraction',
   tagline: 'Reliable open-source solution for Information Extraction',
   url: 'https://mbassijaphet.github.io/pytorch-for-information-extraction/',
-  baseUrl: '/pytorch-for-information-extraction/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
   organizationName: 'MbassiJaphet', // Usually your GitHub org/user name.
